1ea6a1aa0e50d19cffac3d9957a5f6ddd44f20dd
Ryan Dillinger <rd.nss.services@gmail.com>
2019-04-22 14:56:10 -0500

    DID A BUNCH OF STUFF PRAY TO GOD FOR ME & MY CODE

---
+++ src/Actions/PurchaseSeed.cs
@@ -0,0 +1,37 @@
+using System;
+using Trestlebridge.Interfaces;
+using Trestlebridge.Models;
+using Trestlebridge.Models.Plants;
+using Trestlebridge.Models.Facilities;
+
+namespace Trestlebridge.Actions {
+    public class PurchaseStock {
+        public static void CollectInput (Farm farm) {
+            Console.WriteLine ("1. Sunflower");
+            Console.WriteLine ("2. Sesame");
+            Console.WriteLine ("3. Wildflower");
+
+            Console.WriteLine ();
+            // README code
+            Console.WriteLine ("Choose which seed to purchase.");
+            // Old code
+            // Console.WriteLine ("What are you buying today?");
+
+            Console.Write ("> ");
+            string choice = Console.ReadLine ();
+
+            switch (Int32.Parse(choice))
+            {
+                case 1:
+                    ChooseNaturalField.CollectInput(farm, new Sunflower());
+                    break;
+                case 2:
+                    ChoosePlowedField.CollectInput(farm, new Sesame());
+                    break;
+                case 3:
+                    ChooseNaturalField.CollectInput(farm, new Wildflower());
+                    break;
+            }
+        }
+    }
+}
