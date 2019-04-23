c7beaf6d0e1fdc0e0f2f5b4b86088dda10f047cb
Sam Cronin <croninsw@gmail.com>
2019-04-23 15:35:40 -0500

    Added try catch methods to catch user errors

--- src/Actions/PurchaseSeed.cs
+++ src/Actions/PurchaseSeed.cs
@@ -4,33 +4,43 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Plants;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseSeed {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Sunflower");
-            Console.WriteLine ("2. Sesame");
-            Console.WriteLine ("3. Wildflower");
+namespace Trestlebridge.Actions
+{
+    public class PurchaseSeed
+    {
+        public static void CollectInput(Farm farm)
+        {
+            Console.WriteLine("1. Sunflower");
+            Console.WriteLine("2. Sesame");
+            Console.WriteLine("3. Wildflower");
 
-            Console.WriteLine ();
+            Console.WriteLine();
 
-            Console.WriteLine ("Choose which seed to purchase.");
+            Console.WriteLine("Choose which seed to purchase.");
 
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
-
-            switch (Int32.Parse(choice))
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            try
             {
-                case 1:
-                    ChooseFieldType.CollectInput(farm, new Sunflower());
-                    break;
-                case 2:
-                    ChoosePlowedField.CollectInput(farm, new Sesame());
-                    break;
-                case 3:
-                    ChooseNaturalField.CollectInput(farm, new Wildflower());
-                    break;
+                switch (Int32.Parse(choice))
+                {
+                    case 1:
+                        ChooseFieldType.CollectInput(farm, new Sunflower());
+                        break;
+                    case 2:
+                        ChoosePlowedField.CollectInput(farm, new Sesame());
+                        break;
+                    case 3:
+                        ChooseNaturalField.CollectInput(farm, new Wildflower());
+                        break;
+                }
             }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
         }
     }
 }
--- src/Models/Facilities/ChickenHouse.cs
+++ src/Models/Facilities/ChickenHouse.cs
@@ -56,5 +56,6 @@ namespace Trestlebridge.Models.Facilities
             return output.ToString();
         }
 
+
     }
 }
